0ee9756273d16a6d427b10b1aa5ea18ff79c96cb
Ben <bengunton@btinternet.com>
2022-07-16 14:36:54 +0100

    Make http request inside GetGoodDay

--- twitter/twitter.go
+++ twitter/twitter.go
@@ -1,20 +1,48 @@
 package twitter
 
 import (
+	"io"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/bengunton/GoodDay/models"
 )
 
 type TweetFetcher struct {
-	apiKey string
+	bearerToken string
 }
 
 func CreateFetcher() models.Fetcher {
 	key := os.Getenv("BearerToken")
-	return TweetFetcher{apiKey: key}
+	return TweetFetcher{bearerToken: "Bearer " + key}
 }
 
 func (t TweetFetcher) GetGoodDay() string {
-	return t.apiKey
+	client := &http.Client{}
+
+	url := `https://api.twitter.com/2/tweets/search/recent?query="it's%20a%20good%20day%20to"&tweet.fields=created_at&max_results=10`
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+
+	req.Header.Add("Authorization", t.bearerToken)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+
+	return string(body)
 }
