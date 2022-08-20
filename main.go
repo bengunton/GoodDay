1a32b5856d6638ccd3c7ec84653cb25d7670a643
Ben <bengunton@btinternet.com>
2022-08-20 23:02:23 +0100

    Output to text file

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/bengunton/GoodDay/twitter"
 )
@@ -9,5 +11,21 @@ import (
 func main() {
 	t := twitter.CreateFetcher()
 	fmt.Println("It's a good day to...")
-	fmt.Println(t.GetGoodDay())
+
+	goodDay := t.GetGoodDay()
+	fmt.Println(goodDay)
+
+	WriteToFile(goodDay)
+}
+
+func WriteToFile(contents string) {
+	f, err := os.Create("./output/output.txt")
+	if err != nil {
+		log.Print(err)
+	}
+
+	_, err = f.WriteString(contents + "\n")
+	if err != nil {
+		log.Print(err)
+	}
 }
